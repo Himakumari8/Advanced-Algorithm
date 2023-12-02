b45258f6ccff7a8feed858e7cd29d732cd9e4bff
Himakumari8 <124321375+Himakumari8@users.noreply.github.com>
2023-12-02 16:20:02 -0600

    Add files via upload

---
+++ sh6.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+const int kNumOfBits = 32;
+const int kNumOfBytes = kNumOfBits / 8;
+const int kNumOfWords = 64 / kNumOfBits;
+const int kNumOfRounds = 64;
+
+std::vector<uint32_t> round_constants = {
+    // Round constants (unchanged)
+};
+
+uint32_t left_rotate(uint32_t value, int r) {
+    return (value << r) | (value >> (32 - r));
+}
+
+std::string sha256(std::string str) {
+    // Step 1: Preprocess the message and append padding
+    // ...
+
+    // Step 2: Divide the message into 512-bit chunks
+    std::vector<std::string> message_chunks; // Assuming this contains the chunks
+
+    uint32_t h[8] = {0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a, 0x510e527f, 0x9b05688c, 0x1f83d9ab, 0x5be0cd19};
+
+    for (size_t i = 0; i < message_chunks.size(); ++i) {
+        uint32_t w[kNumOfWords];
+
+        // Step 3.1: Break the chunk into 32-bit words
+        // ...
+
+        // Step 3.2: Initialize the hash value for this chunk
+        uint32_t hash[8];
+        std::copy(h, h + 8, hash);
+
+        // Step 3.3: Perform the main computation for this chunk
+        for (int j = 0; j < kNumOfRounds; ++j) {
+            uint32_t s0 = left_rotate(hash[(j + 1) % 8], 7) ^ left_rotate(hash[(j + 1) % 8], 18) ^ (hash[(j + 1) % 8] >> 3);
+            uint32_t s1 = left_rotate(hash[(j + 4) % 8], 17) ^ left_rotate(hash[(j + 4) % 8], 19) ^ (hash[(j + 4) % 8] >> 10);
+
+            uint32_t temp1 = hash[(j + 7) % 8] + s1 + w[j % 16] + round_constants[j];
+            uint32_t temp2 = s0 + left_rotate(temp1, 25);
+
+            hash[(j + 3) % 8] = temp1 + temp2;
+            hash[(j + 7) % 8] = temp1;
+        }
+
+        // Step 3.4: Add the hash value of this chunk to the final hash value
+        for (int j = 0; j < 8; ++j) {
+            h[j] += hash[j];
+        }
+    }
+
+    // Step 4: Convert the final hash value to a hexadecimal string
+    std::string hexadecimal_string = "FinalHashValueInHex"; // Needs implementation
+
+    return hexadecimal_string;
+}
+
+int main() {
+    std::string input_message = "Hello, SHA-256!";
+    std::string hashed_message = sha256(input_message);
+    std::cout << "SHA-256 hash: " << hashed_message << std::endl;
+
+    return 0;
+}
